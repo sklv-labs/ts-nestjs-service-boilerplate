5d605c6b4aaba29924efd64055a0d620d1f21b1f
sokolovgit <sanok.sokolov928100@gmail.com>
2026-01-09 19:55:36 +0200

    feat: test openapi module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { OpenApiModule, OpenApiModuleOptions } from '@sklv-labs/ts-nestjs-openapi';
 
 import { AppControllerTest } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    OpenApiModule.forRootAsync({
+      useFactory: (): OpenApiModuleOptions => ({
+        title: 'My API',
+        description: 'My API description',
+        version: '1.0.0',
+        path: 'api/docs',
+        ui: 'scalar',
+        scalar: {
+          theme: 'default',
+        },
+      }),
+    }),
+  ],
   controllers: [AppControllerTest],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { OpenApiModule } from '@sklv-labs/ts-nestjs-openapi';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const port = process.env.PORT || 3000;
+
+  OpenApiModule.setup(app, {
+    title: 'My API',
+    description: 'My API description',
+    version: '1.0.0',
+    path: 'api/docs',
+    ui: 'scalar',
+    scalar: {
+      theme: 'mars',
+    },
+  });
+
   await app.listen(port);
 
   console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`API documentation is running on: http://localhost:${port}/api/docs`);
 }
 
 void bootstrap();
